8ed3bbe2309efeabd00687fae8feb9a563339706
sewon-x <tylerchoi@naver.com>
2020-07-31 01:13:10 +0900

    20-07-31과제

--- 과제.py
+++ 과제.py
@@ -1,15 +1,4 @@
-import csv
-
-f=open('BostonHousing.csv','r')
-s=f.readlines(13)
-
-for i in range(len(s)):
-    n=s[i].split(",")
-    print(s)
-    
-    sum=0
-    for j in range(len(s)):
-        sum=sum+int(s[j])
+import pandas as pd
 
     print("avg=",sum/len(s))
     print("max=",max(s))
